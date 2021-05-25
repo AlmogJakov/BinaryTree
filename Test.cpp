f197f9c5d1acaf7fed234bdb9f550198cf00e102
AlmogJakov <almog1006@gmail.com>
2021-05-25 13:02:56 +0300

    additional tests were added.

--- Test.cpp
+++ Test.cpp
@@ -10,7 +10,10 @@ using namespace ariel;
 #include <sstream> // stringstream
 #include <string>
 using namespace std;
-//#include <fstream> // ifstream
+
+    ///////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////// add_root test ////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
 
 TEST_CASE("add_root test") {
     stringstream result;
@@ -18,19 +21,38 @@ TEST_CASE("add_root test") {
     tree_of_ints.add_root(1);
     for (int element: tree_of_ints) {result << element;} // this should work like inorder
     CHECK(result.str() == "1");
+    result.str("");
+    /* Root override scenario */
+    tree_of_ints.add_root(2);
+    for (int element: tree_of_ints) {result << element;} // this should work like inorder
+    CHECK(result.str() == "2");
 
     result.str("");
     BinaryTree<double> tree_of_doubles;
     tree_of_doubles.add_root(1.5);
     for (double element: tree_of_doubles) {result << element;} // this should work like inorder
     CHECK(result.str() == "1.5");
+    result.str("");
+    /* Root override scenario */
+    tree_of_doubles.add_root(2.6);
+    for (double element: tree_of_doubles) {result << element;} // this should work like inorder
+    CHECK(result.str() == "2.6");
 
     result.str("");
     BinaryTree<string> tree_of_strings;
-    tree_of_strings.add_root("test_check");
+    tree_of_strings.add_root("root_check");
     for (string element: tree_of_strings) {result << element;} // this should work like inorder
-    CHECK(result.str() == "test_check");
+    CHECK(result.str() == "root_check");
+    result.str("");
+    /* Root override scenario */
+    tree_of_strings.add_root("override_check");
+    for (string element: tree_of_strings) {result << element;} // this should work like inorder
+    CHECK(result.str() == "override_check");
 }
+
+    ///////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////// add_left test ////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
 
 TEST_CASE("add_left test") {
     BinaryTree<int> tree_of_ints;
@@ -47,9 +69,12 @@ TEST_CASE("add_left test") {
     for (int element: tree_of_ints) {result << element << " ";} // this should work like inorder
     CHECK(result.str() == "3 2 1 ");
     result.str("");
-    tree_of_ints.add_left(1, 3);
+    /* Variable override scenario */
+    tree_of_ints.add_left(1, -3);
     for (int element: tree_of_ints) {result << element << " ";} // this should work like inorder
-    CHECK(result.str() == "3 3 1 ");
+    CHECK(result.str() == "3 -3 1 ");
+    /* No-input-found scenario */
+    CHECK_THROWS(tree_of_ints.add_left(20, 1));
 
     result.str("");
     BinaryTree<double> tree_of_doubles;
@@ -62,9 +87,12 @@ TEST_CASE("add_left test") {
     for (double element: tree_of_doubles) {result << element << " ";} // this should work like inorder
     CHECK(result.str() == "3.7 2.6 1.5 ");
     result.str("");
-    tree_of_doubles.add_left(1.5, 3.7);
+    /* Variable override scenario */
+    tree_of_doubles.add_left(1.5, -3.7);
     for (double element: tree_of_doubles) {result << element << " ";} // this should work like inorder
-    CHECK(result.str() == "3.7 3.7 1.5 ");
+    CHECK(result.str() == "3.7 -3.7 1.5 ");
+    /* No-input-found scenario */
+    CHECK_THROWS(tree_of_doubles.add_left(20.2, 1.2));
     
     result.str("");
     BinaryTree<string> tree_of_strings;
@@ -77,10 +105,17 @@ TEST_CASE("add_left test") {
     for (string element: tree_of_strings) {result << element << " ";} // this should work like inorder
     CHECK(result.str() == "grandson child parent ");
     result.str("");
+    /* Variable override scenario */
     tree_of_strings.add_left("parent", "grandson");
     for (string element: tree_of_strings) {result << element << " ";} // this should work like inorder
     CHECK(result.str() == "grandson grandson parent ");
+    /* No-input-found scenario */
+    CHECK_THROWS(tree_of_strings.add_left("other", "other"));
 }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////// add_right test ////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
 
 TEST_CASE("add_right test") {
     BinaryTree<int> tree_of_ints;
@@ -97,9 +132,12 @@ TEST_CASE("add_right test") {
     for (int element: tree_of_ints) {result << element << " ";} // this should work like inorder
     CHECK(result.str() == "1 2 3 ");
     result.str("");
-    tree_of_ints.add_right(1, 3);
+    /* Variable override scenario */
+    tree_of_ints.add_right(1, -3);
     for (int element: tree_of_ints) {result << element << " ";} // this should work like inorder
-    CHECK(result.str() == "1 3 3 ");
+    CHECK(result.str() == "1 -3 3 ");
+    /* No-input-found scenario */
+    CHECK_THROWS(tree_of_ints.add_right(20, 1));
 
     result.str("");
     BinaryTree<double> tree_of_doubles;
@@ -112,9 +150,12 @@ TEST_CASE("add_right test") {
     for (double element: tree_of_doubles) {result << element << " ";} // this should work like inorder
     CHECK(result.str() == "1.5 2.6 3.7 ");
     result.str("");
-    tree_of_doubles.add_right(1.5, 3.7);
+    /* Variable override scenario */
+    tree_of_doubles.add_right(1.5, -3.7);
     for (double element: tree_of_doubles) {result << element << " ";} // this should work like inorder
-    CHECK(result.str() == "1.5 3.7 3.7 ");
+    CHECK(result.str() == "1.5 -3.7 3.7 ");
+    /* No-input-found scenario */
+    CHECK_THROWS(tree_of_doubles.add_right(20.2, 1.2));
     
     result.str("");
     BinaryTree<string> tree_of_strings;
@@ -127,10 +168,17 @@ TEST_CASE("add_right test") {
     for (string element: tree_of_strings) {result << element << " ";} // this should work like inorder
     CHECK(result.str() == "parent child grandson ");
     result.str("");
+    /* Variable override scenario */
     tree_of_strings.add_right("parent", "grandson");
     for (string element: tree_of_strings) {result << element << " ";} // this should work like inorder
     CHECK(result.str() == "parent grandson grandson ");
+    /* No-input-found scenario */
+    CHECK_THROWS(tree_of_strings.add_right("other", "other"));
 }
+
+    ///////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////// preorder test ////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
 
 TEST_CASE("preorder test") {
     BinaryTree<int> tree_of_ints;
@@ -145,8 +193,37 @@ TEST_CASE("preorder test") {
         result << (*it) << " " ;
     }  // prints: 1 2 4 5 3 
     CHECK(result.str() == "1 2 4 5 3 ");
-    //result.str("");
+    result.str("");
+
+    BinaryTree<double> tree_of_doubles;
+    tree_of_doubles.add_root(1.2)
+    .add_left(1.2, 9.8)
+    .add_left(9.8, 4.5)
+    .add_right(9.8, 5.6)
+    .add_right(1.2, 3.4)
+    .add_left(1.2, 2.3);
+    for (auto it=tree_of_doubles.begin_preorder(); it!=tree_of_doubles.end_preorder(); ++it) {
+        result << (*it) << " " ;
+    }  // prints: 1.2 2.3 4.5 5.6 3.4 
+    CHECK(result.str() == "1.2 2.3 4.5 5.6 3.4 ");
+    result.str("");
+
+    BinaryTree<string> tree_of_strings;
+    tree_of_strings.add_root("one")
+    .add_left("one", "nine")
+    .add_left("nine", "four")
+    .add_right("nine", "five")
+    .add_right("one", "three")
+    .add_left("one", "two");
+    for (auto it=tree_of_strings.begin_preorder(); it!=tree_of_strings.end_preorder(); ++it) {
+        result << (*it) << " " ;
+    }  // prints: one two four five three
+    CHECK(result.str() == "one two four five three ");
 }
+
+    //////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////// inorder test ////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////
 
 TEST_CASE("inorder test") {
     BinaryTree<int> tree_of_ints;
@@ -161,8 +238,37 @@ TEST_CASE("inorder test") {
         result << (*it) << " " ;
     }  // prints: 4 2 5 1 3 
     CHECK(result.str() == "4 2 5 1 3 ");
-    //result.str("");
+    result.str("");
+
+    BinaryTree<double> tree_of_doubles;
+    tree_of_doubles.add_root(1.2)
+    .add_left(1.2, 9.8)
+    .add_left(9.8, 4.5)
+    .add_right(9.8, 5.6)
+    .add_right(1.2, 3.4)
+    .add_left(1.2, 2.3);
+    for (auto it=tree_of_doubles.begin_inorder(); it!=tree_of_doubles.end_inorder(); ++it) {
+        result << (*it) << " " ;
+    }  // prints: 4.5 2.3 5.6 1.2 3.4 
+    CHECK(result.str() == "4.5 2.3 5.6 1.2 3.4 ");
+    result.str("");
+
+    BinaryTree<string> tree_of_strings;
+    tree_of_strings.add_root("one")
+    .add_left("one", "nine")
+    .add_left("nine", "four")
+    .add_right("nine", "five")
+    .add_right("one", "three")
+    .add_left("one", "two");
+    for (auto it=tree_of_strings.begin_inorder(); it!=tree_of_strings.end_inorder(); ++it) {
+        result << (*it) << " " ;
+    }  // prints: four two five one three 
+    CHECK(result.str() == "four two five one three ");
 }
+
+    //////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////// postorder test ///////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////
 
 TEST_CASE("postorder test") {
     BinaryTree<int> tree_of_ints;
@@ -177,5 +283,30 @@ TEST_CASE("postorder test") {
         result << (*it) << " " ;
     }  // prints: 4 5 2 3 1 
     CHECK(result.str() == "4 5 2 3 1 ");
-    //result.str("");
+    result.str("");
+
+    BinaryTree<double> tree_of_doubles;
+    tree_of_doubles.add_root(1.2)
+    .add_left(1.2, 9.8)
+    .add_left(9.8, 4.5)
+    .add_right(9.8, 5.6)
+    .add_right(1.2, 3.4)
+    .add_left(1.2, 2.3);
+    for (auto it=tree_of_doubles.begin_postorder(); it!=tree_of_doubles.end_postorder(); ++it) {
+        result << (*it) << " " ;
+    }  // prints: 4.5 5.6 2.3 3.4 1.2 
+    CHECK(result.str() == "4.5 5.6 2.3 3.4 1.2 ");
+    result.str("");
+
+    BinaryTree<string> tree_of_strings;
+    tree_of_strings.add_root("one")
+    .add_left("one", "nine")
+    .add_left("nine", "four")
+    .add_right("nine", "five")
+    .add_right("one", "three")
+    .add_left("one", "two");
+    for (auto it=tree_of_strings.begin_postorder(); it!=tree_of_strings.end_postorder(); ++it) {
+        result << (*it) << " " ;
+    }  // prints: four five two three one 
+    CHECK(result.str() == "four five two three one ");
 }
